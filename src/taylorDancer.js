70c47bcdb7e9ebc6ea73eeba2c11aaa7b2dd19d0
Taylor Hayduk <taylorhayduk@gmail.com>
2015-08-24 18:53:57 -0400

    Refactored dancer, blinkeyDancer, and init to Pseudoclassical

--- src/taylorDancer.js
+++ src/taylorDancer.js
@@ -1,21 +1,19 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  makeDancer.call(this, top, left, timeBetweenSteps);
+  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = this.step;
+  var oldStep = blinkyDancer.step;
 
-  this.step = function(){
+  blinkyDancer.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep.call(this);
+    oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    console.log("I'm stepping")
-    this.$node.toggle();
+    blinkyDancer.$node.toggle();
   };
 
+  return blinkyDancer;
 };
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
